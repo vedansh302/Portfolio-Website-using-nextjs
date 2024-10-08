846b7ead999296153f664fb752d6621c3a22b03c
Vedansh Shrivastava <vedanshwork302@gmail.com>
2024-10-08 16:36:10 +0530

    updates3

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,24 +1,25 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
+import darkSaasLandingPage from "@/assets/images/saveit passmanager1.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import Image from "next/image";
 import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right-link.svg";
+import githubIcon from "@/assets/icons/github.svg";
 import grainImage from "@/assets/images/grain.jpg";
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
 
 const portfolioProjects = [
   {
-    technologies: "Next.js & TypeScript",
-    year: "2022",
-    title: "Dark Saas Landing Page",
+    technologies: "React & JavaScript",
+    year: "2024",
+    title: "SaveIT - Password Manager",
     results: [
       { title: "Enhanced user experience by 40%" },
       { title: "Improved site speed by 50%" },
       { title: "Increased mobile traffic by 35%" },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
+    link: "https://saveit-passwordmanager.vercel.app",
     image: darkSaasLandingPage,
   },
   {
@@ -51,46 +52,46 @@ export const ProjectsSection = () => {
   return (
     <section className="pb-16 lg:py-24">
       <div className="container">
-        <SectionHeader eyebrow="Real-World Results" title="Featured Projects" description="See how I transformed concepts into engaging digital experiences."/>
+        <SectionHeader eyebrow="Real-World Results" title="Featured Projects" description="See how I transformed concepts into engaging digital experiences." />
         <div className="flex flex-col mt-10 md:mt-20 gap-20">
           {portfolioProjects.map((project, projectIndex) => (
-            <Card 
-            key={project.title} 
-            className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky" 
-            style={{
-              top: `calc(64px + ${projectIndex * 40}px`,
-            }}
+            <Card
+              key={project.title}
+              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"
+              style={{
+                top: `calc(64px + ${projectIndex * 40}px`,
+              }}
             >
 
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
-                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold  text-sm text-transparent bg-clip-text">
-                  <span>{project.technologies}</span>
-                  <span>&bull;</span>
-                  <span>{project.year}</span>
-                </div>
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold  text-sm text-transparent bg-clip-text">
+                    <span>{project.technologies}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
 
-                <h3 className="font-serif text-2xl mt-2 md:text-4xl md:mt-5">{project.title}</h3>
-                <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
-                <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                  {project.results.map(result => (
-                    <li key={result.title} className="flex gap-2 text-sm md:text-base text-white/50">
-                      <CheckCircleIcon className="size-5 md:size-6" />
-                      <span>
-                        {result.title}
-                      </span>
-                    </li>
-                  ))}
-                </ul>
-                <a href={project.link} target="blank">
-                  <button className="bg-white text-gray-950 h-12 w-full md:w-auto md:px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
-                    <span>Visit Live Site</span>
-                    <ArrowUpRightIcon className="size-4" />
-                  </button>
-                </a>
+                  <h3 className="font-serif text-2xl mt-2 md:text-4xl md:mt-5">{project.title}</h3>
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map(result => (
+                      <li key={result.title} className="flex gap-2 text-sm md:text-base text-white/50">
+                        <CheckCircleIcon className="size-5 md:size-6" />
+                        <span>
+                          {result.title}
+                        </span>
+                      </li>
+                    ))}
+                  </ul>              
+                    <a href={project.link} target="blank">
+                      <button className="bg-white text-gray-950 h-12 w-full md:w-auto md:px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                        <span>Visit Live Site</span>
+                        <ArrowUpRightIcon className="size-4" />
+                      </button>
+                    </a>               
                 </div>
                 <div className="relative">
-                <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none" />
+                  <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none" />
                 </div>
               </div>
             </Card>
